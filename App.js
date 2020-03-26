220f38fb64f717e0e19e1dea29ab61cf24f9a993
Abdelrhman <abdelrhman@Abdelrhmans-MacBook-Pro.local>
2020-03-26 19:15:25 +0200

    basic context hook

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import IndexScreen from './src/screens/IndexScreen'
+import { BlogProvider } from './src/context/BlogContext'
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Index" component={IndexScreen} 
+          options={{ title: 'BLog' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+    
+  )
+}
